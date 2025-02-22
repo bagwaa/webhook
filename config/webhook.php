22431da31d24d64946dc322fd45a50c082120bba
Richard Bagshaw <richard@bagshaw.co.uk>
2025-02-22 19:18:15 +0000

    Update example wbhook

--- config/webhook.php
+++ config/webhook.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 return [
-    'webhook_url' => 'https://api.bagwaa.dev/webhook',
+    'webhook_url' => 'https://example.com/webhook',
 
     'events' => [
         'entry_saved' => [
