7303101ba62f552eb05c8e724b6538a8d2827ef5
Richard Bagshaw <richard@bagshaw.co.uk>
2025-02-22 12:30:59 +0000

    Enable or disable events

--- config/webhook.php
+++ config/webhook.php
@@ -4,4 +4,12 @@ declare(strict_types=1);
 
 return [
     'webhook_url' => 'https://api.bagwaa.dev/webhook',
+    'webhook_entry_saved_event' => true,
+    'webhook_entry_deleted_event' => true,
+    'webhook_collection_saved_event' => true,
+    'webhook_collection_deleted_event' => true,
+    'webhook_taxonomy_saved_event' => true,
+    'webhook_taxonomy_deleted_event' => true,
+    'webhook_term_saved_event' => true,
+    'webhook_term_deleted_event' => true,
 ];
